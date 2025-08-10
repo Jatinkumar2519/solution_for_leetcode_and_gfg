25aac34ab55d50886afec354a832a737c1b4dbd4
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2025-08-10 10:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (41.89%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    unordered_set<int> set;
-    void solve(string& s,int mask,string num){
-        if(num.length() == s.length()){
-            if(num.back() == '0') return;
-            reverse(num.begin(),num.end());
-            set.insert(stoi(num));
-            return;
-        }
-        if(num.length() > 0 && (num[0] - '0') % 2 != 0) return;
-        for(int i = 0;i < s.length();i++){
-            if(mask & (1 << i)) continue;
-            solve(s,mask | (1 << i),num + s[i]);
-        }
-    }
     bool reorderedPowerOf2(int n) {
-        string num = to_string(n);
-        solve(num,0,"");
-        for(auto& x : set){
-            if(__builtin_popcount(x) == 1) return true;
+        string s = sortDigits(n);
+
+        for(int i = 0; i<31; i++){
+            int power = 1 << i;
+            if(s == sortDigits(power)){
+                return true;
+            }
         }
         return false;
+    }
+    string sortDigits(int num) {
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        return s;
     }
 };
