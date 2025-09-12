839cb7a0888835c56adb9584ef7c15bcae685687
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2025-09-12 13:33:27 +0530

    Time: 2 ms (89.18%) | Memory: 19.2 MB (25.75%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count = 0;
+        for(char ch : s){
+            count += (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+        }
+        return count > 0;
+    }
+};
