bdde6573f6783bf62f80854ca69a63c0130f5239
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-01-10 15:14:33 +0530

    Time: 10 ms (68.89%) | Memory: 22.1 MB (27.27%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int longestIncreasingPath(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        int dirx[] = {-1, 1, 0, 0};
-        int diry[] = {0, 0, -1, 1};
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        function<int(int, int)> solve = [&](int x, int y) -> int {
-            if (dp[x][y] != -1)
-                return dp[x][y];
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int dirx[4] = {-1,0,1,0};
+        int diry[4] = {0,-1,0,1};
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        function<int(int,int)> solve = [&](int i,int j){
+            if(dp[i][j] != -1) return dp[i][j];
             int maxv = 0;
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dirx[d];
-                int ny = y + diry[d];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (mat[x][y] < mat[nx][ny]) {
-                        maxv = max(maxv, 1 + solve(nx, ny));
+            for(int d = 0;d < 4;d++){
+                int nx = i + dirx[d];
+                int ny = j + diry[d];
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
+                    if(matrix[i][j] < matrix[nx][ny]){
+                        maxv = max(maxv,1 + solve(nx,ny));
                     }
                 }
             }
-            return dp[x][y] = maxv;
+            return dp[i][j] = maxv;
         };
-        int maxlen = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                maxlen = max(maxlen, 1 + solve(i, j));
+
+
+        int maxPath = 1;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                maxPath = max(maxPath,1 + solve(i,j));
             }
         }
-        return maxlen;
+        return maxPath;
     }
 };
