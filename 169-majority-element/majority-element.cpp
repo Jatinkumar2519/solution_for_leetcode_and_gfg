7634a34e675a5e607bac761879ae78290117248f
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-01-28 15:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (27.30%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,15 +3,16 @@ public:
     int majorityElement(vector<int>& nums) {
         int major = -1;
         int vote = 0;
+
         for(int i = 0;i < nums.size();i++){
             if(vote == 0){
                 major = nums[i];
             }
 
-            if(major != nums[i]) vote++;
+            if(major == nums[i]) vote++;
             else vote--;
         }
-        
+
         return major;
     }
 };
