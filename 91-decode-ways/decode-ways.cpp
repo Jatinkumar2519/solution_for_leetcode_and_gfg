da1050ffb1619087b0e09ee17b2f85c033b5b536
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-02-07 09:56:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (28.82%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,34 +3,42 @@ public:
     int numDecodings(string s) {
         int n = s.length();
 
-        int dp[101];
-        function<int(int)> solve = [&](int i)->int{
-            if(i == n) return 1;
+        // int dp[101];
+        // function<int(int)> solve = [&](int i)->int{
+        //     if(i == n) return 1;
 
-            if(dp[i] != -1) return dp[i];
+        //     if(dp[i] != -1) return dp[i];
 
-            if(s[i] == '0') return 0;
+        //     if(s[i] == '0') return 0;
 
-            int count = solve(i + 1);
+        //     int count = solve(i + 1);
 
+        //     if(i + 2 <= n && stoi(s.substr(i,2)) <= 26){
+        //         count += solve(i + 2);
+        //     }
 
-            if(i + 2 <= n && stoi(s.substr(i,2)) <= 26){
-                count += solve(i + 2);
+        //     return dp[i] = count;
+        // };
+
+        // memset(dp,-1,sizeof(dp));
+        
+        // return  solve(0);
+
+        vector<int> dp(n + 1);
+        dp[n] = 1;
+
+        for(int i = n - 1;i >= 0;i--){
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
             }
+            
+            dp[i] += dp[i + 1];
+            if(i + 2 <= n && stoi(s.substr(i,2)) <= 26){
+                dp[i] += dp[i + 2];
+            }
+        }
 
-            return dp[i] = count;
-        };
-
-        memset(dp,-1,sizeof(dp));
-        int res = solve(0);
-
-        return (res != INT_MAX) ? res : 0;
-
-        // vector<int> dp(n + 1);
-        // dp[n] = 1;
-
-        // for(int i = n - 1;i >= 0;i--){
-
-        // }
+        return dp[0];
     }
 };
