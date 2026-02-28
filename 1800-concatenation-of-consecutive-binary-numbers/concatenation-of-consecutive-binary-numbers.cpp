19e16768b075ce86237b55bee59bfeda51efc711
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-02-28 12:21:04 +0530

    Time: 44 ms (59.06%) | Memory: 8.1 MB (41.61%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -4,13 +4,11 @@ public:
         int mod = 1e9 + 7;
 
         long long num = 0;
+        int len = 0;
         for(int i = 1;i <= n;i++){
-            int len = log2(i) + 1;
-            
-            // for(int j = 0;j < len;j++){
-                num = (num << len) % mod;
-            // }
+            if((i & (i - 1)) == 0) len++;
 
+            num = (num << len) % mod;
             num = (num + i) % mod;
         } 
 
