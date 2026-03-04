272f1b0f4ae393d11eb74aead98eb93aec37c8e8
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-03-04 23:49:46 +0530

    Time: 46 ms (76.16%) | Memory: 54 MB (72.70%) - LeetSync

---
+++ 2743-debounce/debounce.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let id = null
+    return function(...args) {
+        if(isNaN(id)){
+            id = setTimeout(()=>{
+                fn(...args)
+                id = null
+            },t)
+        }
+        else{
+            clearTimeout(id)
+            
+            id = setTimeout(()=>{
+                fn(...args)
+                id = null
+            },t)
+        }
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
