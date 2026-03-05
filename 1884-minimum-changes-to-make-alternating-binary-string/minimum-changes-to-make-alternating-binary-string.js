417c926018e5225d58736882c7639c5b78bc7970
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-03-05 19:53:36 +0530

    Time: 9 ms (10.87%) | Memory: 60.7 MB (6.52%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let n = s.length
+
+    let s1 = ""
+    for(let i = 0;i < n;i++){
+        if(i % 2 == 0){
+            s1 += '0'
+        }
+        else{
+            s1 += '1'
+        }
+    }
+    let s2 = ""
+    for(let i = 0;i < n;i++){
+        if(i % 2 != 0){
+            s2 += '0'
+        }
+        else{
+            s2 += '1'
+        }
+    }
+
+    let count1 = 0;
+    let count2 = 0;
+
+    for(let i = 0;i < n;i++){
+        count1 += (s1[i] != s[i]);
+        count2 += (s2[i] != s[i]);
+    }
+
+    return Math.min(count1,count2)
+};
