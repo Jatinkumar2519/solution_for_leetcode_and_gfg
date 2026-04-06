d7b02697807f5e9c2f57c07d425cfc4ebac9fdac
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-04-06 21:01:44 +0530

    Time: 20 ms (76.53%) | Memory: 37.8 MB (56.53%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.cpp
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        set<pair<int, int>> check;
-        for (auto& arr : obstacles) {
-            check.insert({arr[0], arr[1]});
+        int dir = 0;
+
+        int dirx[4] = {0,1,0,-1};
+        int diry[4] = {1,0,-1,0};
+
+        int n = commands.size();
+
+        set<pair<int,int>> obs;
+        for(auto& ob : obstacles){
+            obs.insert({ob[0],ob[1]});
         }
-        int x = 0, y = 0;
-        int dir = 0, maxd = 0;
-        int dirx[] = {0, 1, 0, -1};
-        int diry[] = {1, 0, -1, 0};
-        for (int& c : commands) {
-            if (c == -1) {
+
+        int x = 0,y = 0;
+        int maxd = 0;
+
+        for(auto& d : commands){
+            if(d == -2){
+                dir = (dir + 3) % 4;
+            }
+            else if(d == -1){
                 dir = (dir + 1) % 4;
-            } else if (c == -2) {
-                dir = (dir - 1 + 4) % 4;
-            } else {
-                for (int i = 0; i < c; i++) {
-                    if (check.count({x + dirx[dir], y + diry[dir]}) == 0) {
-                        x += dirx[dir];
-                        y += diry[dir];
-                        maxd = max(maxd, x * x + y * y);
-                    } else {
-                        break;
-                    }
+            }
+            else{
+                int k = d;
+
+                while(!obs.count({x + dirx[dir],y + diry[dir]}) && k > 0){
+                    x += dirx[dir];
+                    y += diry[dir];
+                    k--;
                 }
             }
+
+            // cout<< x << " " << y << endl;
+            maxd = max(maxd,x * x + y * y);
         }
+
         return maxd;
     }
 };
