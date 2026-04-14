b7803a15003c968a6b32d189ad67ce1f1c9afc4e
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-04-14 19:26:52 +0530

    Time: 53 ms (49.32%) | Memory: 68.8 MB (37.67%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -17,17 +17,34 @@ public:
         }
 
         m = factp.size();
-        ll dp[101][10001];
+        // ll dp[101][10001];
         
-        function<ll(int,int)> solve = [&](int r,int f)->ll{
-            if(r == n) return 0;
-            if(f == m) return 1ll * 1e12;
+        // function<ll(int,int)> solve = [&](int r,int f)->ll{
+        //     if(r == n) return 0;
+        //     if(f == m) return 1ll * 1e12;
 
-            if(dp[r][f] != -1) return dp[r][f];
-            return dp[r][f] = min(abs(robot[r] - factp[f]) + solve(r + 1,f + 1),solve(r,f + 1));
-        };
+        //     if(dp[r][f] != -1) return dp[r][f];
+        //     return dp[r][f] = min(abs(robot[r] - factp[f]) + solve(r + 1,f + 1),solve(r,f + 1));
+        // };
 
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(0,0);
+
+        vector<vector<ll>> dp(n + 1,vector<ll>(m + 1,0));
+        
+        for(int i = 0;i < n;i++){
+            dp[i][m] = 1e12;
+        }
+        for(int i = 0;i <= m;i++){
+            dp[n][i] = 0;
+        }
+
+        for(int r = n - 1;r >= 0;r--){
+            for(int f = m - 1;f >= 0;f--){
+                dp[r][f] = min(abs(robot[r] - factp[f]) + dp[r + 1][f + 1],dp[r][f + 1]);
+            }
+        }
+
+        return dp[0][0];
     }
 };
