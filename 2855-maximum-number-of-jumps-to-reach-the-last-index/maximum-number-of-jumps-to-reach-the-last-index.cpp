75287e109bcf98b443b3972ccfda794731cd7263
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-05-10 14:53:53 +0530

    Time: 48 ms (10.56%) | Memory: 69.7 MB (9.63%) - LeetSync

--- 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2855-maximum-number-of-jumps-to-reach-the-last-index/maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -3,26 +3,53 @@ public:
     int maximumJumps(vector<int>& nums, int target) {
         int n = nums.size();
         
-        priority_queue<pair<int,int>> pq;
-        pq.push({0,0});
+        // priority_queue<pair<int,int>> pq;
+        // pq.push({0,0});
 
-        vector<int> dist(n,INT_MIN);
-        dist[0] = 0;
-        while(!pq.empty()){
-            auto [cost,node] = pq.top();pq.pop();
+        // vector<int> dist(n,INT_MIN);
+        // dist[0] = 0;
+        // while(!pq.empty()){
+        //     auto [cost,node] = pq.top();pq.pop();
 
-            if(dist[node] > cost) continue;
+        //     if(dist[node] > cost) continue;
             
+        //     for(int i = node + 1;i < n;i++){
+        //         int dif = nums[i] - nums[node];
+
+        //         if(-target <= dif && dif <= target && cost + 1 > dist[i]){
+        //             dist[i] = cost + 1;
+        //             pq.push({cost + 1,i});
+        //         }
+        //     }
+        // }
+
+        // return (dist[n - 1] == INT_MIN ? -1 : dist[n - 1]);
+
+        int dp[1001];
+        
+        function<int(int)> solve = [&](int node)->int{
+            if(node == n - 1) return 0;
+
+            if(dp[node] != -1) return dp[node];
+
+            int maxv = INT_MIN;
             for(int i = node + 1;i < n;i++){
                 int dif = nums[i] - nums[node];
 
-                if(-target <= dif && dif <= target && cost + 1 > dist[i]){
-                    dist[i] = cost + 1;
-                    pq.push({cost + 1,i});
+                if(-target <= dif && dif <= target){
+                    int res = solve(i);
+                    if(res != INT_MIN){
+                        maxv = max(maxv,1 + res);
+                    }
                 }
             }
-        }
 
-        return (dist[n - 1] == INT_MIN ? -1 : dist[n - 1]);
+            return dp[node] = maxv;
+        };
+
+        memset(dp,-1,sizeof(dp));
+        int res = solve(0);
+
+        return (res == INT_MIN ? -1 : res);
     }
 };
