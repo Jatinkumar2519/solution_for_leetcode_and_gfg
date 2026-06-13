fae308534c5a9b780bb25e9d6fc843bd4a2a402e
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-06-14 00:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (10.50%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0) return false;
-        while(n % 4 == 0) n/= 4;
-        return n == 1;
+        if(__builtin_popcount(n) != 1) return false;
+        for(int i = 0;i < 32;i++){
+            if(n & (1 << i)){
+                return (i % 2 == 0);
+            }
+        }
+        return false;
     }
 };
