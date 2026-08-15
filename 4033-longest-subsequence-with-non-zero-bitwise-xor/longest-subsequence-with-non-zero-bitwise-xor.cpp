d293e381f0c953b6202ee147886196c4d4ec25a1
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-08-15 14:54:08 +0530

    Time: 109 ms (5.26%) | Memory: 171.4 MB (4.61%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
         int maxV = 0;
-        for (int b = 0; b < 32; b++) {
+        int maxE = *max_element(nums.begin(),nums.end());
+
+        if(maxE == 0) return 0;
+        int len = log2(maxE);
+
+        for (int b = 0; b <= len; b++) {
 
             int zero = 0, one = 0;
             for (int num : nums) {
