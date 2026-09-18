beb4c700951441b1632c898118620e24a04e9fc9
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-09-18 23:03:03 +0530

    Time: 20 ms (81.66%) | Memory: 16.9 MB (75.44%) - LeetSync

--- 884-k-similar-strings/k-similar-strings.cpp
+++ 884-k-similar-strings/k-similar-strings.cpp
@@ -2,32 +2,22 @@ class Solution {
 public:
     int kSimilarity(string s1, string s2) {
         int n = s1.length();
-        map<__int128, unordered_map<int, int>> dp;
-
-        auto getKey = [&](auto& str) -> __int128 {
-            __int128 key = 0;
-            for (auto& ch : str) {
-                key = key * 10 + (ch - 'a');
-            }
-
-            return key;
-        };
+        unordered_map<string, int> dp;
 
         function<int(string&, int)> solve = [&](string& str, int idx) -> int {
+            while (idx < n && str[idx] == s2[idx])
+                idx++;
+
             if (idx == n)
                 return 0;
 
-            __int128 key = getKey(str);
-            if (dp.count(key) && dp[key].count(idx))
-                return dp[key][idx];
-
-            if (s2[idx] == str[idx])
-                return dp[key][idx] = solve(str, idx + 1);
+            string key = str.substr(idx);
+            if (dp.count(key))
+                return dp[key];
 
             int minv = INT_MAX;
-
             for (int j = idx; j < n; j++) {
-                if (s2[idx] == str[j]) {
+                if (s2[idx] == str[j] && s2[j] == str[idx]) {
 
                     swap(str[idx], str[j]);
 
@@ -37,7 +27,19 @@ public:
                 }
             }
 
-            return dp[key][idx] = minv;
+            if (minv == INT_MAX) {
+                for (int j = idx; j < n; j++) {
+                    if (s2[idx] == str[j]) {
+
+                        swap(str[idx], str[j]);
+
+                        minv = min(minv, 1 + solve(str, idx + 1));
+
+                        swap(str[idx], str[j]);
+                    }
+                }
+            }
+            return dp[key] = minv;
         };
 
         return solve(s1, 0);
