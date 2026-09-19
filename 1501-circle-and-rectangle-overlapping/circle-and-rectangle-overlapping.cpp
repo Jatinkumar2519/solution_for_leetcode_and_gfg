de4adc45745401878081796e532c29fba94240bc
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-09-19 15:43:14 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (2.69%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -20,17 +20,8 @@ public:
             int r = pow(x2 - xCenter,2) + pow(y - yCenter,2);
             if(r <= radius * radius) return true;
         }
-
-        cout<< "not in case 1";
-
+        
         // circle inside the square
-        // vector<int> square = {x1 - radius, y1 - radius, x2 + radius, y2 + radius};
-
-        // int x = xCenter;
-        // int y = yCenter;
-
-        // return ((square[0] <= x && x <= square[2]) && (square[1] <= y && y <= square[3]));
-
         int x = xCenter;
         int y = yCenter;
 
