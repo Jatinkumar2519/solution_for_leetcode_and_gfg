047d1d955c038e69b399675fc88b74b981ab3a3f
Jatin kumar <149255592+Jatinkumar2519@users.noreply.github.com>
2026-09-20 17:54:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum = 0;
+        int n = s.length();
+        
+        for(int i = 0;i < n;i++){
+            sum += (i + 1) * (26 - (s[i] - 'a'));
+        }
+
+        return sum;
+    }
+};
